b850e943c9516aac960796cf519c24b1dbd7ec46
Joel Niklaus <jniklaus@fastforward.ch>
2017-12-04 16:25:32 +0100

    some changes to tower behaviour

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ require('prototype.creep');
 require('prototype.tower');
 require('prototype.spawn');
 
+const towers = require('structure.towers');
+
+
 module.exports.loop = function() {
+
     // check for memory entries of died creeps by iterating over Memory.creeps
     for (let name in Memory.creeps) {
         // and checking if the creep is still alive
@@ -13,19 +17,25 @@ module.exports.loop = function() {
         }
     }
 
-    // for each creeps
+    // for each creep
     for (let name in Game.creeps) {
         // run creep logic
         Game.creeps[name].runRole();
     }
 
+
+    for(let room in Game.rooms)
+        towers.defendMyRoom(room);
+    /*
     // find all towers
     var towers = _.filter(Game.structures, s => s.structureType == STRUCTURE_TOWER);
     // for each tower
     for (let tower of towers) {
         // run tower logic
         tower.defend();
+        //tower.repairStructures();
     }
+    */
 
     // for each spawn
     for (let spawnName in Game.spawns) {
--- prototype.tower.js
+++ prototype.tower.js
@@ -9,3 +9,18 @@ StructureTower.prototype.defend =
             this.attack(target);
         }
     };
+
+StructureTower.prototype.repairStructures =
+    function () {
+        // Only heal if 50% energy left -> spare rest for attacks
+        if (this.energy > 0.5 * this.energyCapacity) {
+            var closestDamagedStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+            });
+            if (closestDamagedStructure) {
+                this.repair(closestDamagedStructure);
+            }
+        }
+    };
+
+
